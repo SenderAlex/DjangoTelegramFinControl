4af31fd00843e26fdd398f168d2c3a84648c6ac0
Alexander Sender <141014024+SenderAlex@users.noreply.github.com>
2025-06-15 19:58:51 +0300

    Update config_ai.py

--- ai_service/config_ai.py
+++ ai_service/config_ai.py
@@ -1,1 +1,2 @@
-AI_TOKEN = 'YOUR_OPENAI_TOKEN'  # Замените на ваш AI_TOKEN
+AI_TOKEN = 'YOUR_OPENAI_TOKEN'  # Замените на ваш API ключ (секретный ключ)
+FOLDER_ID = "YOUR_CATALOG_IDENTIFICATOR"  # Замените на ваш идентификатор каталога
