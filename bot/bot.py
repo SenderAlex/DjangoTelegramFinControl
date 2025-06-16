af47f97ea78a087a54af6e774f98e15ac9d9b76e
Alexander Sender <alexander.sender@gmail.com>
2025-06-16 13:54:51 +0300

    update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -12,8 +12,12 @@ from scheduler import send_daily_summary
 import pytz
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import os
+import sys
 import django
 
+
+# Добавляем корень проекта в PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fincontrol_project.settings')  # Укажите ваш settings
 django.setup()
